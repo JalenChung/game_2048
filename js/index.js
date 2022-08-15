ed618a2849ee30d00c3d8e16b724c1f15102d726
JalenChung <jalen_chung@foxmail.com>
2022-08-15 16:54:57 +0800

    完成基本页面布局，电脑端移动端适配

--- js/index.js
+++ js/index.js
@@ -1,1 +1,88 @@
 let squareBox = $(".squareBox");
+
+let isMousedown = false, isTouchstart = false;
+let direction = undefined;
+let x, y;
+
+// 电脑端滑动方向确定
+squareBox.on("mousedown mouseup", function (e) {
+
+    if (e.type == "mousedown") {
+        console.log("mousedown");
+        isMousedown = true;
+        x = e.pageX;
+        y = e.pageY;
+    }
+
+    if (e.type == "mouseup") {
+        console.log("mouseup");
+        isMousedown = false;
+        direction = undefined;
+    }
+});
+squareBox.mousemove(function (e) {
+
+    if (isMousedown && !direction) {
+
+        if (x - e.pageX > 5) {
+            direction = "l";
+        }
+        if (x - e.pageX < -5) {
+            direction = "r";
+        }
+        if (y - e.pageY < -5) {
+            direction = "d";
+        }
+        if (y - e.pageY > 5) {
+            direction = "u";
+        }
+        console.log(direction)
+    }
+});
+// 移动端滑动方向确定
+squareBox.on("touchstart touchend", function (e) {
+
+    if (e.type == "touchstart") {
+        console.log("touchstart");
+        isTouchstart = true;
+        x = e.originalEvent.touches[0].pageX;
+        y = e.originalEvent.touches[0].pageY;
+    }
+
+    if (e.type == "touchend") {
+        console.log("touchend");
+        isTouchstart = false;
+        direction = undefined;
+    }
+});
+squareBox.on("touchmove", function (e) {
+
+    if (isTouchstart && !direction) {
+
+        if (x - e.originalEvent.touches[0].pageX > 5) {
+            direction = "l";
+        }
+        if (x - e.originalEvent.touches[0].pageX < -5) {
+            direction = "r";
+        }
+        if (y - e.originalEvent.touches[0].pageY < -5) {
+            direction = "d";
+        }
+        if (y - e.originalEvent.touches[0].pageY > 5) {
+            direction = "u";
+        }
+        console.log(direction)
+    }
+
+});
+
+(() => {
+    const htmlObj = document.documentElement;
+    // if (htmlObj.clientWidth > 992) {
+    // htmlObj.style.fontSize = htmlObj.clientWidth / 20 + "px";
+    // }
+    // else {
+    htmlObj.style.fontSize = htmlObj.clientWidth / 10 + "px";
+    // }
+})()
+
